c6286fc9bca1f617527918c2428447a703ed462b
d3nniskd <denniskd@gmail.com>
2022-10-04 23:04:47 +0200

    lidar+sonar werkend

--- projects/p1/bak/nncoded_client(zonder_lidar).py
+++ projects/p1/bak/nncoded_client(zonder_lidar).py
@@ -18,9 +18,8 @@ import parameters as pm
 # lokatie deze file ->     /Users/dennis/Repo/MakeAIWork/simulations/car/control_client/nncoded_client.py
 # lokatie pickeld_model -> /Users/dennis/Repo/MakeAIWork/projects/p1/pickle_model.pkl
 model_sonar = pickle.load(open('../../../projects/p1/pickle_model.pkl', 'rb'))
-model_lidar = pickle.load(open('../../../projects/p1/lidar_model.pkl', 'rb'))
 # dit verderop in de code aanroepen -> pickled_model.predict(X_test)
-    
+
 class NncodedClient:
     def __init__ (self):
         self.steeringAngle = 0
@@ -54,17 +53,62 @@ class NncodedClient:
             if self.model == None:
                 self.model = model_sonar
 
-    def lidarSweep (self): 
-        steeringangle = self.model.predict(np.array([self.lidarDistances]))
-        self.steeringAngle = float(steeringangle[0]) 
-        self.targetVelocity = pm.getTargetVelocity (self.steeringAngle) #steeringAngle (stuurhoek) koppelen aan targetVelocity
+    def lidarSweep (self): # we kijken in dit stukje code steeds naar 2 pionnen. Dit stukje van de code is hetgeen we echt zelf moeten gaan herschrijven
+        nearestObstacleDistance = pm.finity # hier setten we die op 20, dus hij ziet pionnen tot 20m
+        nearestObstacleAngle = 0
+        
+        nextObstacleDistance = pm.finity
+        nextObstacleAngle = 0
 
-        print(steeringangle)
+        for lidarAngle in range (-self.halfApertureAngle, self.halfApertureAngle): # we gaan hier van links naar rechts door de sectoren van de aperture, graad voor graad (dus die 16 sectoren is ergens anders voor)
+            lidarDistance = self.lidarDistances [lidarAngle] # voor elke lidar angle hebben we een distance (=de afstand tot pylon voor deze specifieke hoek)
+            
+            if lidarDistance < nearestObstacleDistance: # als hier blijkt dat de afstand kleiner is dan 20, dan is dit dus interessant
+                nextObstacleDistance =  nearestObstacleDistance # we hebben er hierboven 1 gevonden die dichterbij dan we hadden, en dus maken we de dichtsbijzijnde nu de op 1 na dichtsbijzijnde. Dit is nog steeds belangrijke info die we dus willen bewaren
+                nextObstacleAngle = nearestObstacleAngle
+                
+                nearestObstacleDistance = lidarDistance 
+                nearestObstacleAngle = lidarAngle
+
+            elif lidarDistance < nextObstacleDistance: # hier wordt nog gekeken naar de op-1-na dichtsbijzijnde
+                nextObstacleDistance = lidarDistance
+                nextObstacleAngle = lidarAngle
+           
+        targetObstacleDistance = (nearestObstacleDistance + nextObstacleDistance) / 2 # ik weet de hoek tussen beide pylonnen, en ga daar midden tussendoor rijden
+
+        self.steeringAngle = (nearestObstacleAngle + nextObstacleAngle) / 2 # wordt bepaald door 2 pionnen
+        self.targetVelocity = pm.getTargetVelocity (self.steeringAngle) # bepaald snelheid op basis van stuurhoek
 
     def sonarSweep (self):
         steeringangle = self.model.predict(np.array([self.sonarDistances]))
         self.steeringAngle = float(steeringangle[0]) # koppelen
         self.targetVelocity = pm.getTargetVelocity (self.steeringAngle)
+        # dit is stuk code dat door de NN moet worden bestuurd als ik het goed begrijp
+        # obstacleDistances = [pm.finity for sectorIndex in range (3)]
+        # obstacleAngles = [0 for sectorIndex in range (3)]
+        
+        # for sectorIndex in (-1, 0, 1):
+        #     sonarDistance = self.sonarDistances [sectorIndex]
+        #     # sonarAngle = 2 * self.halfMiddleApertureAngle * sectorIndex
+        #     sonarAngle = 2 * self.halfMiddleApertureAngle * sectorIndex
+            
+        #     if sonarDistance < obstacleDistances [sectorIndex]:
+        #         obstacleDistances [sectorIndex] = sonarDistance
+        #         obstacleAngles [sectorIndex] = sonarAngle
+
+        # if obstacleDistances [-1] > obstacleDistances [0]:
+        #     leftIndex = -1
+        # else:
+        #     leftIndex = 0
+           
+        # if obstacleDistances [1] > obstacleDistances [0]:
+        #     rightIndex = 1
+        # else:
+        #     rightIndex = 0
+           
+        # # self.steeringAngle = (obstacleAngles [leftIndex] + obstacleAngles [rightIndex]) / 2
+        # self.steeringAngle = sks.predictions(self.sonarDistances) # of zoiets
+        
 
     def sweep (self):
         if hasattr (self, 'lidarDistances'): # afhankelijk van de aangeboden data, wordt hier bepaald of we lidar of sonar gaan gebruiken
--- projects/p1/scikit_lidar.py
+++ projects/p1/scikit_lidar.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn import preprocessing
 
 path_to_file = 'datasets/car_lidar/default.samples'
 
@@ -21,11 +27,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 print(X_train)
 print(y_train)
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+model = LinearRegression()
 
-regressor.fit(X_train, y_train)
+# from sklearn.preprocessing import StandardScaler
+# scaler = StandardScaler()
+# scaler.fit(X_train)
 
-print(regressor.intercept_)
+model.fit(X_train, y_train)
 
-print(regressor.coef_)
+# making predictions
+predictions = model.predict(X_test)
+
+# model evaluation
+print(
+  'mean_squared_error : ', mean_squared_error(y_test, predictions))
+print(
+  'mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+
+# opslaan van model
+import pickle
+pickle.dump(model, open('lidar_model.pkl', 'wb'))
