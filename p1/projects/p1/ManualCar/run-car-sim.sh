a55d059a0011fab404ddaf37ef0b117acc4b2cfd
d3nniskd <denniskd@gmail.com>
2022-10-11 09:55:42 +0200

    mitw1-laatste

--- p1/projects/p1/ManualCar/run-car-sim.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export LIBGL_ALLOW_SOFTWARE=1
-python3 simpylc/simulations/car_manual_flex_track/world.py
--- p1/simulations/car/control_client/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-pickled_model = pickle.load(open('../../projects/p1/pickle_model.pkl', 'rb'))
-
-# model = "pickle_model.pkl"
-# with open(model, 'rb') as file:
-#     pickle_model = pickle.load(file)
-
-# print(pickle_model)
